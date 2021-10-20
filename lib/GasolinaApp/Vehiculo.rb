946ee6f64e4c592660a8ab3ca940b2e781b0b41e
paula1999 <pvillanunez@correo.ugr.es>
2021-10-20 16:24:02 +0200

    Añado la correccion

--- lib/GasolinaApp/Vehiculo.rb
+++ lib/GasolinaApp/Vehiculo.rb
@@ -4,9 +4,9 @@ require 'date'
 
 module GasolinaApp
     class Vehiculo 
-        attr_reader marca, matricula, fecha_matricula, modelo, motorizacion, km_recorridos, consumo_ciudad, consumo_carretera, consumo_mixto
+        attr_reader marca, matricula, fecha_matricula, modelo, motorizacion, consumo_ciudad, consumo_carretera, consumo_mixto
 
-        def initialize (marca, matricula, motorizacion, km_recorridos, fecha_matricula = nil, modelo = nil)
+        def initialize (marca, matricula, motorizacion)
         end
 
         # Calcular fecha de matrícula a partir de la matrícula
